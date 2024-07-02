fddad70e4eb7b2cf248e74a12a9180426759f2da
R-S-Associates <nbtemily@gmail.com>
2024-07-02 03:58:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function openTab(tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+}
 
