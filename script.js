c8110ed74edb895fc9faef82734f2480bdffb273
R-S-Associates <nbtemily@gmail.com>
2024-07-02 04:47:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ document.addEventListener("DOMContentLoaded", function() {
   // Set 'About Company' tab as active by default
   document.getElementById('about-company').classList.add('active');
   document.getElementById('about-company').style.display = 'block';
+
+  // Add event listeners to tab links
+  var tablinks = document.getElementsByClassName("tablink");
+  for (var i = 0; i < tablinks.length; i++) {
+    tablinks[i].addEventListener("click", function() {
+      var tabName = this.getAttribute("data-tab");
+      openTab(tabName);
+    });
+  }
 });
 
 function openTab(tabName) {
