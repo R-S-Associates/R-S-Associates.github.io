0aebf160891664d02d935e9928617dc3bd49344f
R-S-Associates <nbtemily@gmail.com>
2024-07-02 04:42:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function openTab(tabName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tab-content");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  document.getElementById(tabName).style.display = "block";
-}
+<script src="script.js"></script>
+  <script>
+    // JavaScript to set 'About Company' tab as active by default
+    document.addEventListener("DOMContentLoaded", function() {
+      document.getElementById('about-company').classList.add('active'); // Ensure 'active' class is added
+      document.getElementById('about-company').style.display = 'block'; // Ensure 'display' is set to 'block'
+    });
+
+    function openTab(tabName) {
+      var tabcontent = document.getElementsByClassName("tab-content");
+      for (var i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+      }
+      document.getElementById(tabName).style.display = "block";
+    }
+  </script>
